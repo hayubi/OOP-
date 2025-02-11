60fde97b8cfb8f8626e00dbc0842972f7b147220
hayubi <hayubi53@gmail.com>
2025-02-11 13:20:02 +0500

    Add files via upload

--- lab4/q5.cpp
+++ lab4/q5.cpp
@@ -14,3 +14,69 @@ Requirements:
      `isManager()`: Returns `true` if the employee's department is "Management".
      `displayDetails()`: Displays the employee's details.
 5. Create a few `Employee` objects and test the methods.*/
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Employee 
+{
+    private:
+        int employeeID;
+        string name;
+        string department;
+        double salary;
+
+    public:
+        Employee() : salary(0.0) {}
+
+        Employee(int pid, string pName, string pDept, double pSalary) : employeeID(pid), name(pName), department(pDept), salary(pSalary) {}
+
+        void giveBonus(double amount) 
+        {
+            salary = salary + amount;
+        }
+
+        bool isManager() 
+        {
+            if (department == "Management")
+                return true;
+            return false;
+        }
+
+        void displayDetails() 
+        {
+            cout << "\nID: " << employeeID << endl;
+            cout << "Name: " << name << endl;
+            cout << "Department: " << department << endl;
+            cout << "Salary: " << salary << endl;
+        }
+};
+
+int main() 
+{
+    Employee emp1(121, "Manahil", "HR", 78000);
+    Employee emp2(122, "Armeen", "Management", 55600);
+    Employee emp3(123, "Marium", "Sales", 36000);
+    emp1.displayDetails();
+    emp2.displayDetails();
+    emp3.displayDetails();
+
+    cout << "\nBonus of 8000$ given to employee 2" << endl;
+    emp2.giveBonus(8000);
+    emp2.displayDetails();
+
+    cout << "\nIs emp2 a manager? ";
+    if (emp2.isManager())
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+
+    cout << "Is emp3 a manager? ";
+    if (emp3.isManager())
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+
+}
